aeb8b66957415903a631df53da985b9f41fd2394
Anthony <amcmullen9291@gmail.com>
2021-11-08 20:42:39 -0500

    get randomState from txt file without using loop

--- src/AnthonyWeek5/HangMan2.java
+++ src/AnthonyWeek5/HangMan2.java
@@ -1,26 +1,36 @@
 package AnthonyWeek5;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HangMan2 {
     public static void main(String[] args) throws IOException {
+        FileReader fileReader;
+        BufferedReader reader;
+
         try {
-            File file = new File("src/AnthonyWeek5/Words.txt");
-            FileInputStream fis = new FileInputStream(file);
-            byte[] data = new byte[(int) file.length()];
-            fis.read(data);
-            fis.close();
-            String statesList = new String(data, "UTF-8");
-            System.out.println(statesList);
+//            Scanner s = new Scanner(new File("src/AnthonyWeek5/Words.txt"));
+//            ArrayList<String> statesList = new ArrayList<String>();
+            Random r = new Random();
+            int low = 1;
+            int high = 50;
+            int randomState = r.nextInt(high-low) + low;
+
+            fileReader = new FileReader("src/AnthonyWeek5/Words.txt");
+            reader = new BufferedReader(fileReader);
+            String lineIn = reader.readLine();
+            String line32 = Files.readAllLines(Paths.get("src/AnthonyWeek5/Words.txt")).get(randomState);
+            reader.close();
+            System.out.println(line32);
         }catch(IOException e){
             e.printStackTrace();
         }
-
     }
 }
 
